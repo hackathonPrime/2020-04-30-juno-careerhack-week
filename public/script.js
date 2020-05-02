ec6f037d3bd4f1d43cd001dd8f7f963974cb5fb8
Nick Reyno <nickrenoe@gmail.com>
2020-05-02 17:04:12 -0400

    user auth functionality

--- public/script.js
+++ public/script.js
@@ -1,61 +1,80 @@
 // Gets the data and outputs it to the `out` div
 function fetchAndPrintData() {
-  fetch('/api/data')
-    .then(data => data.json())
-    .then(json => {
-      document.getElementById('out').textContent = JSON.stringify(json)
-    } );
+	fetch("/api/data")
+		.then((data) => data.json())
+		.then((json) => {
+			document.getElementById("out").textContent = JSON.stringify(json);
+		});
 }
 
 // Submits the form and refreshes the data
 function submitForm() {
-  event.preventDefault();
+	event.preventDefault();
 
-  const title = event.target.title.value;
-  const description = event.target.description.value;
+	const title = event.target.title.value;
+	const description = event.target.description.value;
 
-  fetch('/api/data', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ title, description })
-  })
-  .then(() => {
-    console.log(JSON.stringify({ title, description }))
-    fetchAndPrintData()
-  });
-
+	fetch("/api/data", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ title, description }),
+	}).then(() => {
+		console.log(JSON.stringify({ title, description }));
+		fetchAndPrintData();
+	});
 }
 
 // run this on load
 fetchAndPrintData();
 
-
-
-
 //user auth
 
 //signup
-const signupForm = document.querySelector('#signupForm');
-const signupLink = document.querySelector('#signupLink')
+const signupForm = document.querySelector("#signupForm");
+const signupLink = document.querySelector("#signupLink");
+const loginForm = document.querySelector("#loginForm");
+const loginLink = document.querySelector("#loginLink");
+const logoutLink = document.querySelector("#logout");
 
-signupLink.addEventListener('click',()=>{
-  signupForm.style.display = 'block';
-})
-signupForm.addEventListener('submit',(e)=>{
-e.preventDefault();
+// sign up show modal
+signupLink.addEventListener("click", () => {
+	signupForm.style.display = "block";
+});
+loginLink.addEventListener("click", () => {
+	loginForm.style.display = "block";
+});
+// sign up function
+signupForm.addEventListener("submit", (e) => {
+	e.preventDefault();
 
-// get user info
-const email = signupForm['signupEmail'].value;
-const password = signupForm['signupPassword'].value;
+	// get user info
+	const email = signupForm["signupEmail"].value;
+	const password = signupForm["signupPassword"].value;
 
-// sign up this user
+	// sign up this user in firebase
+	auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+		const modal = document.querySelector("#modalSignup");
+		signupForm.style.display = "none";
+		signupForm.reset();
+	});
+});
 
-auth.createUserWithEmailAndPassword(email,password).then(cred=>{
-console.log(cred.user);
-const modal = document.querySelector('#modalSignup')
-signupForm.style.display = 'none';
-signupForm.reset();
-})
-})
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = loginForm['loginEmail'].value;
+  const password = loginForm['loginPassword'].value;
+  auth.signInWithEmailAndPassword(email, password).then((cred)=>{
+console.log(cred.user, 'user has logged in');
+loginForm.style.display = "none";
+loginForm.reset();
+  })
+});
+
+logoutLink.addEventListener("click", (e) => {
+	e.preventDefault();
+	auth.signOut().then(() => {
+		console.log("user has signed out");
+	});
+});
